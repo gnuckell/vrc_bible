f0f77476e36e1f797c2431b7ddae5848bf2b9a1b
Liam Wofford <27900656+gnuckell@users.noreply.github.com>
2025-03-12 16:10:46 -0500

    Added basic bible spawner

---
+++ scripts/BibleSpawnerExitZone.cs
@@ -0,0 +1,29 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class BibleSpawnerExitZone : UdonSharpBehaviour
+{
+    [SerializeField] private Transform spawn_transform;
+    [SerializeField] private GameObject prefab_spawn;
+    private GameObject current_spawn;
+
+    void Start()
+    {
+        Destroy(spawn_transform.GetChild(0).gameObject);
+        SpawnNewBible();
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject != current_spawn) return;
+        SpawnNewBible();
+    }
+
+    public void SpawnNewBible()
+    {
+        current_spawn = Instantiate(prefab_spawn, spawn_transform.position, spawn_transform.rotation);
+    }
+}
