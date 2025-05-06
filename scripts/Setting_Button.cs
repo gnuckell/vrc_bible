1db081301bda8890c6593d5d149076990a82db38
Liam Wofford <27900656+snotbane@users.noreply.github.com>
2025-05-06 15:55:05 -0500

    Added Setting_Button which disables when not owned

---
+++ scripts/Setting_Button.cs
@@ -0,0 +1,22 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Setting_Button : BibleDeedListener
+{
+    [HideInInspector] public UnityEngine.UI.Button button;
+
+	protected override void Start()
+	{
+        button = GetComponent<UnityEngine.UI.Button>();
+
+		base.Start();
+	}
+
+	public override void Refresh()
+	{
+        button.interactable = deed.is_owner;
+	}
+}
